d0eea7151b8df5b5bbe159dc190bcb94492bc7c5
arthurjf <arthur.310100@hotmail.com>
2023-04-14 18:56:40 -0300

    Spinner para selecionar o tema da forca

--- app/src/main/java/br/com/arthurjf/jogodaforca/view/GameplayActivity.kt
+++ app/src/main/java/br/com/arthurjf/jogodaforca/view/GameplayActivity.kt
@@ -1,11 +1,12 @@
-package br.com.arthurjf.jogodaforca
+package br.com.arthurjf.jogodaforca.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.arthurjf.jogodaforca.R
 
-class MainActivity : AppCompatActivity() {
+class GameplayActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_gameplay)
     }
 }
---
+++ app/src/main/java/br/com/arthurjf/jogodaforca/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.com.arthurjf.jogodaforca.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.arthurjf.jogodaforca.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
