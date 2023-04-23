4b0d821c5463e05401afefe097ea56b73cbd5f22
arthurjf <arthur.310100@hotmail.com>
2023-04-23 17:39:20 -0300

    Utilizando o Coroutines no Spinner de temas

--- app/src/main/java/br/com/arthurjf/jogodaforca/data/ThemesRepository.kt
+++ app/src/main/java/br/com/arthurjf/jogodaforca/data/ThemesRepository.kt
@@ -2,16 +2,17 @@ package br.com.arthurjf.jogodaforca.data
 
 import android.content.Context
 import com.google.gson.Gson
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class ThemesRepository(private val context: Context) {
-    fun getThemes(callback: (themes: Themes) -> Unit) {
-        Thread {
+    suspend fun getThemes(): Themes {
+        return withContext(Dispatchers.Default){
             val gson = Gson()
             val json = context.assets.open("words.json")
                 .bufferedReader()
                 .use { it.readText() }
-            val themes = gson.fromJson(json, Themes::class.java)
-            callback.invoke(themes)
-        }.start()
+            gson.fromJson(json, Themes::class.java)
+        }
     }
 }
--- app/src/main/java/br/com/arthurjf/jogodaforca/view/ui/viewmodel/MainViewModel.kt
+++ app/src/main/java/br/com/arthurjf/jogodaforca/view/ui/viewmodel/MainViewModel.kt
@@ -3,15 +3,23 @@ package br.com.arthurjf.jogodaforca.view.ui.viewmodel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import br.com.arthurjf.jogodaforca.data.ThemesRepository
 import br.com.arthurjf.jogodaforca.data.Themes
+import br.com.arthurjf.jogodaforca.data.ThemesRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-class MainViewModel(private val wordsRepository: ThemesRepository) : ViewModel() {
+class MainViewModel(private val themesRepository: ThemesRepository) : ViewModel() {
     val themesLiveData = MutableLiveData<Themes>()
 
     fun getThemes() {
-        wordsRepository.getThemes() { themes ->
-            themesLiveData.postValue(themes)
+        CoroutineScope(Dispatchers.Main).launch {
+            val themes = withContext(Dispatchers.Default) {
+                themesRepository.getThemes()
+            }
+
+            themesLiveData.value = themes
         }
     }
 
