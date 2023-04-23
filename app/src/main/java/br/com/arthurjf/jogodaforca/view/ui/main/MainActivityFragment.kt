b8e25d762a132066a2b5830f832553a51bb20283
arthurjf <arthur.310100@hotmail.com>
2023-04-23 17:14:42 -0300

    Import nao utilizado do Spinner removido

--- app/src/main/java/br/com/arthurjf/jogodaforca/view/ui/main/MainActivityFragment.kt
+++ app/src/main/java/br/com/arthurjf/jogodaforca/view/ui/main/MainActivityFragment.kt
@@ -7,7 +7,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import android.widget.Spinner
 import br.com.arthurjf.jogodaforca.R
 import com.jaredrummler.materialspinner.MaterialSpinner
 
